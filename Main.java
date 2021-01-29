18a45f23f1a4feada1e25b7157d16047ee6fcede
hscragg <hayden.scragg@patelhs.org>
2021-01-29 17:30:25 +0000

    added

--- Main.java
+++ Main.java
@@ -4,12 +4,12 @@ class Main {
   public static void main(String[] args) 
   {
     Scanner scan = new Scanner(System.in);
-    System.out.println("Something One");
+    System.out.println("How old is your mom");
     String name = scan.next();
-    System.out.println("Something Two");
+    System.out.println("How old is your dad");
     int currentTwo = scan.nextInt();
-    System.out.println("Something Three");
-    int optionThree = scan.nextInt();
+    System.out.println("How old are you");
+    int currentThree = scan.nextInt();
 
 
 
