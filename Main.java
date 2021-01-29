0505c4021e939997e4296df0d9bd34f7610a3890
hscragg <hayden.scragg@patelhs.org>
2021-01-29 17:28:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Something One");
+    String name = scan.next();
+    System.out.println("Something Two");
+    int currentTwo = scan.nextInt();
+    System.out.println("Something Three");
+    int optionThree = scan.nextInt();
+
+
+
+
+
+
+
+  }
+}
